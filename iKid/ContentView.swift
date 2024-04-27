3282cce0c4b12b20fc07380624b2ead5c788b1f5
Shiina Sugioka <shiina12@uw.edu>
2024-04-27 15:37:47 -0700

    init repo

---
+++ iKid/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iKid
+//
+//  Created by stlp on 4/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iKid/iKidApp.swift
@@ -0,0 +1,17 @@
+//
+//  iKidApp.swift
+//  iKid
+//
+//  Created by stlp on 4/27/24.
+//
+
+import SwiftUI
+
+@main
+struct iKidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
